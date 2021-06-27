34e16ac5d577f0639349de0f37e47806eef6e5ef
mercybassey <udohmercy911@gmail.com>
2021-06-27 21:54:18 +0000

    added a loading text to show and hide after successful login

--- javascript/sign-in.js
+++ javascript/sign-in.js
@@ -4,15 +4,22 @@ const password = document.getElementById('password');
 
 
 
-const BASE_URL = 'https://maniera-dev.herokuapp.com/api/'
+const BASE_URL = 'https://maniera-dev.herokuapp.com/api/';
+
+//----signin loader---
+const loading = document.getElementById('loading');
+loading.style.display = 'none';
 
 
 form.addEventListener('submit', async e => {
     e.preventDefault();
+
+    loading.style.display = 'block';
 
     let response = await axios.post(`${BASE_URL}auth/signin`, {
         email: email.value,
         password: password.value
     })
     localStorage.setItem('token', response.data.token);
+    loading.style.display = 'none';
 })
