95bda5e65dc3eefaf7d44eb9d4b76ad4c07b438c
NoobMaester <joshstilez@gmail.com>
2021-07-04 18:10:29 +0200

    Cart Commit

---
+++ javascript/cart.js
@@ -0,0 +1,21 @@
+/*var numbers = document.getElementById('box');
+for (i=0; i<10; i++){
+    var span = document.createElement('span');
+    span.textContent = i;
+    numbers.appendChild(span);
+}
+var num = numbers.getElementsByTagName('span');
+var index = 0;
+
+function nextNum(){
+    num[index].style.display = 'none';
+    index = (index + 1) % num.length;
+    num[index].style.display = 'initial';
+    }
+        
+function prevNum(){
+    num[index].style.display = 'none';
+    index = (index - 1 + num.length) % num.length;
+    num[index].style.display = 'initial';        
+}
+*/
