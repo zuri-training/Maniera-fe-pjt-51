864eb15e29dd18d87318cc60fcf07d84405a2aa0
NoobMaester <joshstilez@gmail.com>
2021-07-04 20:10:46 +0200

    increment function on unit price

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,21 +1,32 @@
-/*var numbers = document.getElementById('box');
-for (i=0; i<10; i++){
-    var span = document.createElement('span');
-    span.textContent = i;
-    numbers.appendChild(span);
-}
-var num = numbers.getElementsByTagName('span');
-var index = 0;
 
-function nextNum(){
-    num[index].style.display = 'none';
-    index = (index + 1) % num.length;
-    num[index].style.display = 'initial';
-    }
-        
-function prevNum(){
-    num[index].style.display = 'none';
-    index = (index - 1 + num.length) % num.length;
-    num[index].style.display = 'initial';        
+//DECLARING VARIABLES
+var qty = document.getElementById('box');
+var unitPrice = document.getElementById('up');
+var counter = 1;
+
+
+
+//INCREMENT and DECREMENT FXs
+const nextNum = () => {
+    counter++;
+    console.log(counter)
+    qty.innerHTML = counter;
+
+    let finalPrice = counter * 114;
+    console.log(`The final price would be ${finalPrice}`)
+    unitPrice.innerHTML = finalPrice;
 }
-*/
+const prevNum = () => {
+    counter--;
+    console.log(counter);
+    qty.innerHTML = counter;
+
+    let finalPrice = counter * 114;
+    console.log(`The final price would be ${finalPrice}`);
+    unitPrice.innerHTML = finalPrice;
+}
+
+
+const remove = () => {
+    console.log('Feature coming soon...')
+}
