a31246487def5fb2632b60cec402d7aa69062f7e
Peter Okerulu <oduchep@gmail.com>
2021-07-15 18:22:56 +0100

    debugging again...

--- javascript/make-request.js
+++ javascript/make-request.js
@@ -1,34 +1,30 @@
-const dropdownMenus = document.querySelector('.display-select-input');
-const optionContainer = document.querySelector('.select-control');
-const optionItems = document.querySelectorAll('.option-items');
-const allOpenSelect = document.querySelectorAll('.display-select-input');
+const dropdownMenus = document.querySelector(".display-select-input");
+const optionContainer = document.querySelector(".select-control");
+const optionItems = document.querySelectorAll(".option-items");
+const allOpenSelect = document.querySelectorAll(".display-select-input");
 
 const dropdownMenu = () => {
-    dropdownMenus.classList.toggle('show');
-    console.log('I am clicked');
+	dropdownMenus.classList.toggle("show");
+	console.log("I am clicked");
 };
 
-optionContainer.addEventListener('click', dropdownMenu);
+optionContainer.addEventListener("click", dropdownMenu);
 
 for (const selectOption of optionItems) {
-    selectOption.addEventListener('click', function () {
-        console.log('okay');
-        if (!this.classList.contains('selected')) {
-            this.parentNode
-                .querySelector('.option-items.selected')
-                .classList.remove('selected');
-            this.classList.add('selected');
-            this.closest('.display-select-input').querySelector(
-                '.option-display'
-            ).textContent = this.textContent;
-        }
-    });
+	selectOption.addEventListener("click", function () {
+		console.log("okay");
+		if (!this.classList.contains("selected")) {
+			this.parentNode.querySelector(".option-items.selected").classList.remove("selected");
+			this.classList.add("selected");
+			this.closest(".display-select-input").querySelector(".option-display").textContent = this.textContent;
+		}
+	});
 }
 
-window.addEventListener('click', function (e) {
-    for (const closeAll of allOpenSelect) {
-        if (!closeAll.contains(e.target)) {
-            closeAll.classList.remove('show');
-        }
-    }
+window.addEventListener("click", function (e) {
+	for (const closeAll of allOpenSelect) {
+		if (!closeAll.contains(e.target)) {
+			closeAll.classList.remove("show");
+		}
+	}
 });
